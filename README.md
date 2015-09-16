5e74d4a7eeeda6357deb0a304a7db76c8f220fbd
PatrickW <magetron1@gmail.com>
2015-09-16 23:50:10 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Brainfuck
+
+Brainfuck !!!!!!!
+
+So Amazing !!!!!!!
---
+++ brainfuck.cpp
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define TOKENS "><+-.,[]"
+
+#define CODE_SEGMENT_SIZE 30000
+#define STACK_SEGMENT_SIZE 1000
+#define DATA_SEGMENT_SIZE 30000
+
+typedef void (*Callback)(void);
+
+struct {
+    char cs[CODE_SEGMENT_SIZE];
+    long ip;   
+    
+    char ss[STACK_SEGMENT_SIZE];
+    long sp;
+    
+    char ds[DATA_SEGMENT_SIZE];
+    long bp;
+    
+    Callback fn[128];
+} vm;
+
+void vm_forward() {
+    vm.bp = (vm.bp + 1) % DATA_SEGMENT_SIZE;
+}
+
+void vm_backward() {
+    vm.bp = (vm.bp + DATA_SEGMENT_SIZE - 1) % DATA_SEGMENT_SIZE;
+}
+
+void vm_increment() {
+    vm.ds[vm.bp]++;
+}
+
+void vm_decrement() {
+    vm.ds[vm.bp]--;
+}
+
+void vm_input() {
+    vm.ds[vm.bp] = getchar();
+}
+
+void vm_output() {
+    putchar(vm.ds[vm.bp]);
+}
+
+void vm_while_entry() {
+    if (vm.ds[vm.bp]) {
+        vm.ss[vm.sp] = vm.ip - 1;
+        vm.sp++;
+    } else {
+        int c = 1;
+        for (vm.ip++; vm.cs[vm.ip] && c; vm.ip++) {
+            if (vm.cs[vm.ip] == '[') {
+                c++;
+            } else if (vm.cs[vm.ip] == ']') {
+                c--;
+            }
+        }
+    }
+}
+
+void vm_while_exit() {
+    if (vm.ds[vm.bp]) {
+        vm.sp--;
+        vm.ip = vm.ss[vm.sp];
+    }
+}
+
+void setup() {
+    int c;
+    int i;
+    
+    memset(&vm, 0, sizeof(vm));
+    vm.fn['>'] = vm_forward;
+    vm.fn['<'] = vm_backward;
+    vm.fn['+'] = vm_increment;
+    vm.fn['-'] = vm_decrement;
+    vm.fn['.'] = vm_output;
+    vm.fn[','] = vm_input;
+    vm.fn['['] = vm_while_entry;
+    vm.fn[']'] = vm_while_exit;
+    
+    for (i = 0; (c = getchar()) != EOF;) {
+        if (strchr(TOKENS, c)) {
+            vm.cs[i] = c;
+            i++;
+        }
+    }
+}
+
+void run() {
+    while (vm.cs[vm.ip]) {
+        vm.fn[vm.cs[vm.ip]]();
+        vm.ip++;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {  
+        freopen(argv[1], "r", stdin);  
+    }  
+    
+    setup();  
+    run();  
+    
+    return 0;  
+}
